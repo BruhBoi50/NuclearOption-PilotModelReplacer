7ab806c32f90df5e68edcbaca2b13be3d2ce5123
BruhBoi50 <amuqeet1001@gmail.com>
2026-05-31 07:45:50 +0300

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,207 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+
+namespace PilotModelReplacer
+{
+    [BepInPlugin("com.bruhboi.pilotmodelreplacer", "Pilot Model Replacer", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static AssetBundle CustomBundle;
+        public static GameObject CustomModelPrefab;
+
+        public static ConfigEntry<bool> ConfigPlayerOnly;
+        public static ConfigEntry<string> ConfigBundleName;
+
+
+        private void Awake()
+        {
+            //Setup configuration
+
+            ConfigPlayerOnly = Config.Bind("General", 
+                                           "OnlyChangePlayer", false,
+                                           "If enabled, the mod will only replace the player's pilot model. Otherwise, it replaces" +
+                                           " all pilots. Doesn't work well though");
+
+            ConfigBundleName = Config.Bind("Model Settings",
+                                           "AssetBundleFileName",
+                                           "cj_pilot",
+                                           "The name of the bundle inside the mod folder");
+
+
+
+            //Locate and load the AssetBundle
+            string modFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string bundlePath = Path.Combine(modFolder, "assets", ConfigBundleName.Value);
+
+            if (File.Exists(bundlePath))
+            {
+                CustomBundle = AssetBundle.LoadFromFile(bundlePath);
+                //Load the prefab of the given name
+                CustomModelPrefab = CustomBundle.LoadAsset<GameObject>("pilot");
+                Logger.LogInfo("Custom player model asset bundle loaded successfully");
+            }
+            else
+            {
+                Logger.LogError("Could not find bundle of name: " + ConfigBundleName.Value);
+
+                /*
+                AssetBundle anyBundle = FindAnyBundle(modFolder, "pilot");
+
+                if(anyBundle == null)
+                {
+                    Logger.LogError("Bundles Not found!");
+                    return;
+                }
+
+                CustomBundle = anyBundle;
+                */
+                return;
+            }
+
+            //Initialize Harmony
+            var harmony = new Harmony("com.bruhboi.pilotmodelreplacer");
+            harmony.PatchAll();
+        }
+
+        public static void SwapModel(SkinnedMeshRenderer originalModel, SkinnedMeshRenderer newModel)
+        {
+
+            Debug.Log("[PilotReplacer] Replacing model now...");
+
+            Material mat = newModel.material;
+            Shader gameShader = Shader.Find(mat.shader.name);
+            mat.shader = gameShader;
+
+            FixBoneOrder(originalModel);
+            originalModel.material = newModel.material;
+            originalModel.sharedMesh = newModel.sharedMesh;
+
+        }
+
+        static AssetBundle FindAnyBundle(string path, string prefabName)
+        {
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string filePath in files)
+            {
+
+                AssetBundle bundle = AssetBundle.LoadFromFile(filePath);
+                if (bundle == null) continue;
+
+
+                if (bundle.Contains(prefabName))
+                {
+                    GameObject prefab = bundle.LoadAsset<GameObject>(prefabName);
+                    if (prefab != null)
+                    {
+                        Debug.Log("Found prefab" + prefabName + " in path");
+
+                        // Return this bundle path
+                        return bundle;
+                    }
+                }
+
+                bundle.Unload(true);
+            }
+
+            return null;
+        }
+
+        static void FixBoneOrder(SkinnedMeshRenderer renderer)
+        {
+            Transform[] bonesCopy = renderer.bones;
+
+            for (int i = bonesCopy.Length - 3; i < bonesCopy.Length; i++)
+            {
+                Transform temp = bonesCopy[i];
+                bonesCopy[i] = bonesCopy[i - 3];
+                bonesCopy[i - 3] = temp;
+            }
+
+            renderer.bones = bonesCopy;
+        }
+
+    }
+
+    //Target the PilotDismounted script
+    [HarmonyPatch(typeof(PilotDismounted), "Awake")]
+    public class PilotPatchA
+    {
+        [HarmonyPostfix]
+        public static void Postfix(PilotDismounted __instance)
+        {
+            Debug.Log("[PilotReplacer] Patch triggered: Dismounted");
+
+
+            if (Plugin.ConfigPlayerOnly.Value && __instance.Networkplayer == null)
+            {
+                Debug.Log("[PilotReplacer] Target not player. Skipping");
+                return;
+            }
+            if (Plugin.CustomModelPrefab == null)
+            {
+                Debug.LogError("[PilotReplacer] Prefab not found");
+                return;
+            }
+            
+            SkinnedMeshRenderer originalModel = __instance.GetComponentInChildren<SkinnedMeshRenderer>();
+            SkinnedMeshRenderer newModel = Plugin.CustomModelPrefab.GetComponentInChildren<SkinnedMeshRenderer>();
+
+            if (originalModel == null)
+            {
+                Debug.LogError("[PilotReplacer] Could not find model in object!");
+                return;
+            }
+
+            Plugin.SwapModel(originalModel, newModel);
+
+        }
+
+    }
+
+    
+
+    [HarmonyPatch(typeof(Pilot), "Pilot_OnInitialize", MethodType.Normal)]
+    public class PilotPatchB
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Pilot __instance)
+        {
+            Debug.Log("[PilotReplacer] Patch triggered: Pilot");
+
+            if (Plugin.ConfigPlayerOnly.Value && __instance.player == null && __instance.GetComponent<GLOC>() == null)
+            {
+
+                Debug.Log("[PilotReplacer] Target not player. Skipping");
+                return;
+
+            }
+
+
+            if (Plugin.CustomModelPrefab == null)
+            {
+                Debug.LogError("[PilotReplacer] Prefab not found");
+                return;
+            }
+
+            SkinnedMeshRenderer originalModel = __instance.GetComponentInChildren<SkinnedMeshRenderer>();
+            SkinnedMeshRenderer newModel = Plugin.CustomModelPrefab.GetComponentInChildren<SkinnedMeshRenderer>();
+
+            if (originalModel == null)
+            {
+                Debug.LogError("[PilotReplacer] Could not find model in object!");
+                return;
+            }
+
+            Plugin.SwapModel(originalModel, newModel);
+
+        }
+
+    }
+
+}
